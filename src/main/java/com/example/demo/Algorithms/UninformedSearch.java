a11fb9ce3ea07c2821b506fa3cf310b573015b28
Engy <engeousthepotionmaster@gmail.com>
2022-10-21 02:21:20 +0200

    uninformed search edit such that no duplicate state can enter the fringe

--- src/main/java/com/example/demo/Algorithms/UninformedSearch.java
+++ src/main/java/com/example/demo/Algorithms/UninformedSearch.java
@@ -9,27 +9,42 @@ import java.util.HashSet;
 
 public class UninformedSearch {
     public static ArrayList<String> solve(String initialPuzzle, IFringe fringe) {
+
+        //set containing explored states.
         HashSet<String> visited = new HashSet<>();
+
+        //set keeping track of states inside the fringe.
+        HashSet<String> fringeSet = new HashSet<>();
+
         State initialState = new State(initialPuzzle, null);
+
         State goalState = null;
+
         fringe.push(initialState);
+        fringeSet.add(initialPuzzle);
+
         while(!fringe.empty()) {
+
             State currState = fringe.pop();
+            visited.add(currState.getNumbers());
+            fringeSet.remove(currState.getNumbers());
+
             if(currState.isGoalState()) {
                 goalState = currState;
                 break;
             }
-            String numbers = currState.getNumbers();
-            System.out.println("Current State:" + numbers);
-            if(visited.contains(numbers)) continue;
-            visited.add(numbers);
+
             ArrayList<String> nextStates = currState.generateAdjacent();
             for(String nextState : nextStates) {
-                System.out.println(nextState);
-                State state = new State(nextState, currState);
-                fringe.push(state);
+                if(!visited.contains(nextState) && !fringeSet.contains(nextState)) {
+                    State state = new State(nextState, currState);
+                    fringe.push(state);
+                    fringeSet.add(nextState);
+                }
             }
+
         }
+
         ArrayList<String> solution = new ArrayList<>();
         State currState = goalState;
         while(currState != null) {
